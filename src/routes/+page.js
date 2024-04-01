8d1f074433629ecf9079c3dc8b39008b245bbe30
aayushi363 <aayushi363@gmail.com>
2024-04-01 13:25:18 -0400

    dataset added

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -2,7 +2,7 @@ import * as d3 from 'd3';
 
 export async function load({ fetch }) {
 	// load the csv file as a string
-	const res = await fetch('/mlb-hitters-2023.csv');
+	const res = await fetch('/dataset.csv');
 	const text = await res.text();
 	// parse the string into an array of objects.
 	// d3.autoType will automatically detect the types of the columns
